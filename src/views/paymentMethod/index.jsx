908ee0547ab90fcdab01e7ade808be416befb6fe
ali <md.ali2001@hotmail.com>
2022-07-27 22:30:41 -0700

    gf

--- src/views/paymentMethod/index.jsx
+++ src/views/paymentMethod/index.jsx
@@ -1,38 +1,42 @@
 import React from 'react'
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
 import { useNavigate } from "react-router-dom";
+import { Button, Typography, Stack, TextField } from "@mui/material";
 
 
 function PaymentMethod() {
     const navigate = useNavigate();
 
   return (
-    <div>
-      <Button
-        color="primary"
-        variant="contained"
-        // onClick={() => navigate("")}
-      >
-        Cash
-      </Button>
-    <br/>
-      <Button
-        color="primary"
-        variant="contained"
-        onClick={() => navigate("/posScreen/paymentMethod/payment")}
-      >
-        Credit
-      </Button>
-      <br/>
+    
 
-      <Button
-        color="primary"
-        variant="contained"
-        onClick={() => navigate("/posScreen/paymentMethod/crypto")}
+<div 
+      style={{width: "1150px",
+        position: "absolute",
+        top:"130px",
+         height: "270px",
+          left: "300px"}}
       >
-        Crypto Coin
-      </Button>
-    </div>
+
+
+        <Button
+        sx={{ width: "180px", margin: '6px', height: '100px', border: "2px solid black", position: "absolute", left: "200px" }} variant="contained" color="info" size="small"  > CASH
+        
+        </Button>
+
+
+        <Button 
+        sx={{ width: "180px", margin: '6px', height: '100px', border: "2px solid black", position: "absolute", left: "400px" }} variant="contained" color="info" size="small" onClick={() => navigate("/posScreen/paymentMethod/payment")} > CREDIT
+        </Button>
+
+
+        <Button 
+        sx={{ width: "180px", margin: '6px', height: '100px', border: "2px solid black", position: "absolute", left: "600px" }} variant="contained" color="info" size="small" onClick={() => navigate("/posScreen/paymentMethod/crypto")} >  CRYPTO COIN
+        </Button>
+
+      </div>
+
+
   )
 }
 
